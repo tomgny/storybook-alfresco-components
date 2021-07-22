40a776792d487070be396afeef7a4701aa5257d9
Tomasz Gnyp <wirkod@gmail.com>
2021-07-22 11:47:27 +0200

    Added inputs to card-view component, removed unnecesary scripts from package.json

--- src/app/mycardview/mycardview.component.ts
+++ src/app/mycardview/mycardview.component.ts
@@ -15,31 +15,21 @@
  * limitations under the License.
  */
 
-import { Component, OnInit, ElementRef, ViewChild, OnDestroy, Input, DoCheck } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, OnDestroy, Input } from '@angular/core';
 import {
-    CardViewTextItemModel,
-    CardViewDateItemModel,
-    CardViewDatetimeItemModel,
-    CardViewBoolItemModel,
-    CardViewIntItemModel,
-    //CardViewFloatItemModel,
-    CardViewKeyValuePairsItemModel,
-    CardViewSelectItemModel,
     CardViewUpdateService,
-    CardViewMapItemModel,
     UpdateNotification,
-    //DecimalNumberPipe,
-    CardViewArrayItemModel,
     CardViewItem
 } from '@alfresco/adf-core';
-import { of, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
+    selector: 'app-mycardview',
     templateUrl: './mycardview.component.html',
     styleUrls: ['./mycardview.component.scss']
 })
-export class MycardviewComponent implements OnInit, OnDestroy, DoCheck {
+export class MycardviewComponent implements OnInit, OnDestroy {
 
     @ViewChild('console', { static: true }) console: ElementRef;
 
@@ -56,10 +46,10 @@ export class MycardviewComponent implements OnInit, OnDestroy, DoCheck {
     inputDisplayNoneOption: boolean = true;
 
     @Input()
-    editable: boolean = false;
+    inputEditable: boolean = false;
 
     @Input()
-    inputMultiValueSeparator: string = '.';
+    inputMultiValueSeparator: string = ',';
 
     @Input()
     inputProperties: CardViewItem[] = [];
@@ -67,22 +57,13 @@ export class MycardviewComponent implements OnInit, OnDestroy, DoCheck {
     @Input()
     inputUseChipsForMultiValueProperty: boolean = true;
 
-    @Input()
-    testValueArray: number[] = [];
-
-    isEditable = true;
-    properties: CardViewItem[] = [];
     logs: string[];
-    showClearDateAction = false;
-    showNoneOption = false;
 
     private onDestroy$ = new Subject<boolean>();
 
     constructor(private cardViewUpdateService: CardViewUpdateService,
-                //private decimalNumberPipe: DecimalNumberPipe
                 ) {
         this.logs = [];
-        this.createCard();
     }
 
     respondToCardClick() {
@@ -95,122 +76,9 @@ export class MycardviewComponent implements OnInit, OnDestroy, DoCheck {
             .subscribe(this.onItemChange.bind(this));
     }
 
-    ngDoCheck() {
-      console.log("CHANGE DOCHECK");
-      console.log(`Current state of editable is ${this.editable}`);
-      console.log(`Current state of inputCopyToClipboardAction is ${this.inputCopyToClipboardAction}`);
-      console.log(`Current state of inputMultiValueSeparator is ${this.inputMultiValueSeparator}`);
-
-
-      // this.createCard();
-    }
-
     ngOnDestroy() {
         this.onDestroy$.next(true);
         this.onDestroy$.complete();
-    }
-
-    createCard() {
-        this.properties = [
-            new CardViewTextItemModel({
-                label: 'CardView Text Item - Multivalue (chips)',
-                value: [1, 2, 3, 4],
-                key: 'name',
-                default: 'default bar',
-                multiline: true,
-                multivalued: true,
-                icon: 'icon',
-                editable: this.editable
-            }),
-            new CardViewDateItemModel({
-                label: 'CardView Date Item - Multivalue (chips)',
-                value: [new Date(1983, 11, 24, 10, 0, 30)],
-                key: 'date',
-                default: new Date(1983, 11, 24, 10, 0, 30),
-                format: 'shortDate',
-                editable: this.editable,
-                multivalued: true
-            }),
-            new CardViewDatetimeItemModel({
-              label: 'CardView Datetime Item - Multivalue (chips)',
-              value: [new Date(1983, 11, 24, 10, 0, 0)],
-              key: 'datetime',
-              default: new Date(1983, 11, 24, 10, 0, 0),
-              format: 'short',
-              editable: this.editable,
-              multivalued: true
-            }),
-            new CardViewBoolItemModel({
-                label: 'Agree to all terms and conditions',
-                value: true,
-                key: 'boolean',
-                default: false,
-                editable: this.editable
-            }),
-            new CardViewIntItemModel({
-                label: 'CardView Int Item',
-                value: 213,
-                key: 'int',
-                default: 1,
-                editable: this.editable
-            }),
-            /*
-            new CardViewFloatItemModel({
-                label: 'CardView Float Item - Multivalue (chips)',
-                value: [9.9],
-                key: 'float',
-                default: 0.0,
-                editable: this.editable,
-                multivalued: true,
-                pipes: [{ pipe: this.decimalNumberPipe }]
-            }),
-            */
-            new CardViewKeyValuePairsItemModel({
-                label: 'CardView Key-Value Pairs Item',
-                value: [{ name: 'hey', value: 'you' }, { name: 'hey', value: 'you' }],
-                key: 'key-value-pairs',
-                editable: this.editable
-            }),
-            new CardViewSelectItemModel({
-                label: 'CardView Select Item',
-                value: 'one',
-                options$: of([{ key: 'one', label: 'One' }, { key: 'two', label: 'Two' }]),
-                key: 'select',
-                editable: this.editable
-            }),
-            new CardViewMapItemModel({
-                label: 'My map',
-                value: new Map([['999', 'My Value']]),
-                key: 'map',
-                default: 'default map value'
-            }),
-            new CardViewTextItemModel({
-                label: 'This is clickable ',
-                value: 'click here',
-                key: 'click',
-                default: 'click here',
-                editable: this.editable,
-                clickable: true,
-                icon: 'close',
-                clickCallBack: () => {
-                    this.respondToCardClick();
-                }
-            }),
-            new CardViewArrayItemModel({
-                label: 'CardView Array of items',
-                value: of([
-                    { icon: 'directions_bike', value: 'Zlatan' },
-                    { icon: 'directions_bike', value: 'Lionel Messi' },
-                    { value: 'Mohamed', directions_bike: 'save' },
-                    { value: 'Ronaldo' }
-                ]),
-                key: 'array',
-                icon: 'edit',
-                default: 'Empty',
-                noOfItemsToDisplay: 2,
-                editable: this.editable
-            })
-        ];
     }
 
     onItemChange(notification: UpdateNotification) {
@@ -222,24 +90,5 @@ export class MycardviewComponent implements OnInit, OnDestroy, DoCheck {
 
         this.logs.push(`[${notification.target.label}] - ${value}`);
         this.console.nativeElement.scrollTop = this.console.nativeElement.scrollHeight;
-    }
-
-    toggleEditable() {
-        this.editable = !this.editable;
-        this.createCard();
-    }
-
-    toggleClearDate() {
-        this.showClearDateAction = !this.showClearDateAction;
-    }
-
-    toggleNoneOption() {
-        this.showNoneOption = !this.showNoneOption;
-    }
-
-    reset() {
-        this.editable = true;
-        this.createCard();
-        this.logs = [];
     }
 }
--- src/app/mycardview/mycardview.stories.ts
+++ src/app/mycardview/mycardview.stories.ts
@@ -1,8 +1,9 @@
 import { ContentModule } from '@alfresco/adf-content-services';
-import { CardViewTextItemModel, CoreModule, MaterialModule } from '@alfresco/adf-core';
+import {  CardViewArrayItemModel, CardViewBoolItemModel, CardViewDateItemModel, CardViewDatetimeItemModel, CardViewIntItemModel, CardViewKeyValuePairsItemModel, CardViewMapItemModel, CardViewSelectItemModel, CardViewTextItemModel, CoreModule, MaterialModule } from '@alfresco/adf-core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { of } from 'rxjs';
 import { APP_ROUTES } from '../app.routes';
 import { AppCommonModule } from '../components/common/common.module';
 import { MycardviewComponent } from './mycardview.component';
@@ -38,9 +39,95 @@ const dataSource = [
     multiline: true,
     multivalued: true,
     icon: 'icon',
+    editable: true,
+}),
+new CardViewDateItemModel({
+    label: 'CardView Date Item - Multivalue (chips)',
+    value: [new Date(1983, 11, 24, 10, 0, 30)],
+    key: 'date',
+    default: new Date(1983, 11, 24, 10, 0, 30),
+    format: 'shortDate',
+    editable: true,
+    multivalued: true
+}),
+new CardViewDatetimeItemModel({
+  label: 'CardView Datetime Item - Multivalue (chips)',
+  value: [new Date(1983, 11, 24, 10, 0, 0)],
+  key: 'datetime',
+  default: new Date(1983, 11, 24, 10, 0, 0),
+  format: 'short',
+  editable: true,
+  multivalued: true
+}),
+new CardViewBoolItemModel({
+    label: 'Agree to all terms and conditions',
+    value: true,
+    key: 'boolean',
+    default: false,
     editable: true
 }),
+new CardViewIntItemModel({
+    label: 'CardView Int Item',
+    value: 213,
+    key: 'int',
+    default: 1,
+    editable: true
+}),
+new CardViewKeyValuePairsItemModel({
+    label: 'CardView Key-Value Pairs Item',
+    value: [{ name: 'hey', value: 'you' }, { name: 'hey', value: 'you' }],
+    key: 'key-value-pairs',
+    editable: true
+}),
+new CardViewSelectItemModel({
+    label: 'CardView Select Item',
+    value: 'one',
+    options$: of([{ key: 'one', label: 'One' }, { key: 'two', label: 'Two' }]),
+    key: 'select',
+    editable: true
+}),
+new CardViewMapItemModel({
+    label: 'My map',
+    value: new Map([['999', 'My Value']]),
+    key: 'map',
+    default: 'default map value'
+}),
+new CardViewTextItemModel({
+    label: 'This is clickable ',
+    value: 'click here',
+    key: 'click',
+    default: 'click here',
+    editable: true,
+    clickable: true,
+    icon: 'close'
+}),
+new CardViewArrayItemModel({
+    label: 'CardView Array of items',
+    value: of([
+        { icon: 'directions_bike', value: 'Zlatan' },
+        { icon: 'directions_bike', value: 'Lionel Messi' },
+        { value: 'Mohamed', directions_bike: 'save' },
+        { value: 'Ronaldo' }
+    ]),
+    key: 'array',
+    icon: 'edit',
+    default: 'Empty',
+    noOfItemsToDisplay: 2,
+    editable: true
+})
 ]
+
+const emptyCardViewTextItem = [new CardViewTextItemModel({
+    label: 'CardView Text Item - Multivalue (chips)',
+    value: [],
+    key: 'name',
+    default: 'default bar',
+    multiline: true,
+    multivalued: true,
+    icon: 'icon',
+    editable: true
+  })
+];
 
 const Template: Story<MycardviewComponent> = (args) => ({
   props: {
@@ -48,15 +135,38 @@ const Template: Story<MycardviewComponent> = (args) => ({
   }
 })
 
-export const FirstStory = Template.bind({});
-FirstStory.args = {
+export const DefaultStory = Template.bind({});
+DefaultStory.args = {
   inputCopyToClipboardAction: true,
   inputDisplayClearAction: true,
   inputDisplayEmpty: true,
   inputDisplayNoneOption: true,
-  editable: true,
+  inputEditable: true,
   inputProperties: dataSource,
   inputMultiValueSeparator: ',',
   inputUseChipsForMultiValueProperty: true,
-  //testValueArray: [1,2,3,4,5,6,7]
+}
+
+export const DefaultStoryPropsOff = Template.bind({});
+DefaultStoryPropsOff.args = {
+  inputCopyToClipboardAction: false,
+  inputDisplayClearAction: false,
+  inputDisplayEmpty: false,
+  inputDisplayNoneOption: false,
+  inputEditable: false,
+  inputProperties: dataSource,
+  inputMultiValueSeparator: ',',
+  inputUseChipsForMultiValueProperty: false,
+}
+
+export const EmptyCardViewTexItem = Template.bind({});
+EmptyCardViewTexItem.args = {
+  inputCopyToClipboardAction: true,
+  inputDisplayClearAction: true,
+  inputDisplayEmpty: true,
+  inputDisplayNoneOption: true,
+  inputEditable: false,
+  inputProperties: emptyCardViewTextItem,
+  inputMultiValueSeparator: ',',
+  inputUseChipsForMultiValueProperty: false,
 }
