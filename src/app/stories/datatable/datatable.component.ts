4394fd23273bb93ba20d814d24f19da08c703faa
Tomasz Gnyp <wirkod@gmail.com>
2021-07-27 17:11:00 +0200

    added all inputs and a few stories for them

--- src/app/stories/datatable/datatable.component.ts
+++ src/app/stories/datatable/datatable.component.ts
@@ -1,4 +1,4 @@
-import { ObjectDataTableAdapter } from '@alfresco/adf-core';
+import { ObjectDataTableAdapter, ShowHeaderMode } from '@alfresco/adf-core';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,19 +7,133 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./datatable.component.scss']
 })
 export class DatatableComponent implements OnInit {
-
+  /**
+   * Toggles the data actions column.
+   */
   @Input()
-  stickyHeader: boolean = false;
+  actions: boolean = false;
 
+  /**
+   * Position of the actions dropdown menu. Can be "left" or "right".
+   */
+  @Input()
+  actionsPosition: 'right' | 'left' = 'right';
+
+  /**
+   * If set to true, the actions button will only be visible on row hover.
+   */
+  @Input()
+  actionsVisibleOnHover: boolean = false;
+
+  /**
+   * Flag that indicate if the datatable allow the use facet widget search for filtering.
+   */
+  @Input()
+  allowFiltering: boolean = false;
+
+  /**
+   * The columns that the datatable will show.
+   */
+  @Input()
+  columns: any[] = [];
+
+  /**
+   * Toggles custom context menu for the component.
+   */
+  @Input()
+  contextMenu: boolean = false;
+
+  /**
+   * Data source for the table
+   */
   @Input()
   data = new ObjectDataTableAdapter();
 
+  /**
+   * Selects the display mode of the table. Can be "list" or "gallery".
+   */
   @Input()
-  multiSelect: boolean = true;
+  display: 'list' | 'gallery' = 'list';
 
-  constructor() { }
+  /**
+   * Fallback image for rows where the thumbnail is missing.
+   */
+  @Input()
+  fallbackThumbnail: string;
 
-  ngOnInit(): void {
-  }
+  /**
+   * Flag that indicates if the datatable is in loading state and needs to show the loading template (see the docs to learn how to configure a loading template).
+   */
+  @Input()
+  loading: boolean = false;
 
+  /**
+   * Toggles multiple row selection, which renders checkboxes at the beginning of each row.
+   */
+  @Input()
+  multiselect: boolean = false;
+
+  /**
+   * Flag that indicates if the datatable should show the "no permission" template.
+   */
+  @Input()
+  noPermission: boolean = false;
+
+  /**
+   * Custom resolver function which is used to parse dynamic column objects see the docs to learn how to configure a resolverFn.
+   */
+  //@Input()
+  //resolverFn = this.resolverFunction;
+
+  /**
+   * Should the items for the row actions menu be cached for reuse after they are loaded the first time?
+   */
+  @Input()
+  rowMenuCacheEnabled: boolean = true;
+
+  /**
+   * The inline style to apply to every row. See NgStyle docs for more details and usage examples.
+   */
+  @Input()
+  rowStyle: { [key: string]: any };
+
+  /**
+   * The CSS class to apply to every row.
+   */
+  @Input()
+  rowStyleClass: string = '';
+
+  /**
+   * The rows that the datatable will show.
+   */
+  @Input()
+  rows: any[] = [];
+
+  /**
+   * Row selection mode. Can be none, single or multiple. For multiple mode, you can use Cmd (macOS) or Ctrl (Win) modifier key to toggle selection for multiple rows.
+   */
+  @Input()
+  selectionMode: 'single' | 'multiple' | 'none' = 'multiple';
+
+  /**
+   * Toggles the header.
+   */
+  @Input()
+  showHeader: ShowHeaderMode.Always | ShowHeaderMode.Data | ShowHeaderMode.Never = ShowHeaderMode.Always;
+
+  /**
+   * Define the sort order of the datatable. Possible values are : [created, desc], [created, asc], [due, desc], [due, asc]
+   */
+  @Input()
+  sorting: ['created', 'desc'] | ['created', 'asc'] | ['due', 'desc'] | ['due', 'asc'] = ['created', 'desc'];
+
+  /**
+   * Toggles the sticky header mode.
+   */
+  @Input()
+  stickyHeader: boolean = true;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/stories/datatable/datatable.stories.ts
+++ src/app/stories/datatable/datatable.stories.ts
@@ -1,4 +1,7 @@
-import { CoreModule, ObjectDataTableAdapter } from '@alfresco/adf-core';
+import { ContentModule } from '@alfresco/adf-content-services';
+import { CoreModule, DisplayMode, MaterialModule, ObjectDataTableAdapter, ShowHeaderMode } from '@alfresco/adf-core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { APP_ROUTES } from '../../app.routes';
@@ -18,105 +21,131 @@ export default {
         }),
         CoreModule.forRoot(),
         AppCommonModule,
-    // ContentModule.forRoot(),
-    // MaterialModule,
-      ],
-    }),
+        ContentModule.forRoot(),
+        MaterialModule,
+        BrowserAnimationsModule,
+        CommonModule
+      ]
+    })
   ]
 } as Meta;
 
-const dataSource = new ObjectDataTableAdapter(
-  [
-    {
-      id: 1,
-      firstName: 'Name #1',
-      lastName: 'Lastname #1',
-      icon: 'material-icons://folder_open'
-    },
-    {
-      id: 2,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 3,
-      firstName: 'Name #3',
-      lastName: 'Lastname #3',
-      icon: 'material-icons://alarm'
-    },
-    {
-      id: 4,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 5,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 6,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 7,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 8,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 9,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 10,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 11,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 12,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-    {
-      id: 13,
-      firstName: 'Name #2',
-      lastName: 'Lastname #2',
-      icon: 'material-icons://accessibility'
-    },
-  ]
-);
+const dataSource = new ObjectDataTableAdapter([
+  {
+    id: 1,
+    firstName: 'Name #1',
+    lastName: 'Lastname #1',
+    icon: 'material-icons://folder_open'
+  },
+  {
+    id: 2,
+    firstName: 'Name #2',
+    lastName: 'Lastname #2',
+    icon: 'material-icons://accessibility'
+  },
+  {
+    id: 3,
+    firstName: 'Name #3',
+    lastName: 'Lastname #3',
+    icon: 'material-icons://alarm'
+  },
+  {
+    id: 4,
+    firstName: 'Name #4',
+    lastName: 'Lastname #4',
+    icon: 'material-icons://accessibility'
+  },
+  {
+    id: 5,
+    firstName: 'Name #5',
+    lastName: 'Lastname #5',
+    icon: 'material-icons://alarm'
+  }
+]);
+
+const dataSourceWithoutOneIcon = new ObjectDataTableAdapter([
+  {
+    id: 1,
+    firstName: 'Name #1',
+    lastName: 'Lastname #1',
+    icon: 'material-icons://folder_open'
+  },
+  {
+    id: 2,
+    firstName: 'Name #2',
+    lastName: 'Lastname #2'
+  }
+]);
 
 const Template: Story<DatatableComponent> = (args) => ({
   props: {
     ...args
   }
-})
+});
 
 export const DefaultStory = Template.bind({});
 DefaultStory.args = {
+  actions: false,
+  actionsPosition: 'right',
+  actionsVisibleOnHover: false,
+  allowFiltering: false,
+  //columns: [1,2],
+  contextMenu: false,
   data: dataSource,
-  stickyHeader: true,
-  multiSelect: false
-}
+  display: DisplayMode.List,
+  fallbackThumbnail: '',
+  loading: false,
+  multiselect: false,
+  noPermission: false,
+  //resolverFn: null,
+  rowMenuCacheEnabled: true,
+  rowStyle: '',
+  rowStyleClass: '',
+  //rows: [1,2],
+  selectionMode: 'single',
+  showHeader: ShowHeaderMode.Always,
+  sorting: ['created', 'desc'],
+  stickyHeader: true
+};
+
+export const HiddenHeader = Template.bind({});
+HiddenHeader.args = {
+  showHeader: ShowHeaderMode.Never,
+  data: dataSource
+};
+
+export const RowStyle = Template.bind({});
+RowStyle.args = {
+  rowStyle: { 'font-style': 'italic', 'background-color': 'gold' },
+  data: dataSource
+};
+
+export const RowStyleClass = Template.bind({});
+RowStyleClass.args = {
+  rowStyleClass: 'adf-container-full-width',
+  data: dataSource
+};
+
+export const GaleryDisplay = Template.bind({});
+GaleryDisplay.args = {
+  display: 'gallery',
+  stickyHeader: false,
+  data: dataSource
+};
+
+export const FallbackThumbnail = Template.bind({});
+FallbackThumbnail.args = {
+  fallbackThumbnail: '../../../alfresco-24x24.png',
+  data: dataSourceWithoutOneIcon
+};
+
+export const NoData = Template.bind({});
+NoData.args = {
+  data: undefined
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+  data: dataSource
+};
