446ce64b488634b9cf87d8b1f9e9fcf3f91a6724
michu-dunat <kontaktmichaldunat@gmail.com>
2021-08-18 16:06:05 +0200

    [AAE-5691] use factory for generating notifications

--- src/app/stories/notification-history/notification-history.stories.ts
+++ src/app/stories/notification-history/notification-history.stories.ts
@@ -1,4 +1,5 @@
-import { CoreModule, NotificationModel, NOTIFICATION_TYPE } from '@alfresco/adf-core';
+import { CoreModule, info, warning, error } from '@alfresco/adf-core';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
@@ -19,7 +20,8 @@ export default {
         }),
         CoreModule.forRoot(),
         AppCommonModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatIconModule
       ]
     })
   ],
@@ -32,100 +34,31 @@ export default {
       options: ['above', 'below'],
       control: { type: 'radio' }
     },
-    notifications: { table: { disable: true } },
+    notifications: { table: { disable: true } }
   }
 } as Meta;
 
-const notification1 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification2 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification3 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification4 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification5 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification6 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification7 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification8 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification9 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification10 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
-const notification11 = {
-  messages: ['Hey'],
-  datetime: new Date(),
-  initiator: { displayName: 'System', key: 'system' },
-  type: NOTIFICATION_TYPE.INFO
-} as NotificationModel;
-
 const notifications = [
-  notification1,
-  notification2,
-  notification3,
-  notification4,
-  notification5,
-  notification6,
-  notification7,
-  notification8,
-  notification9,
-  notification10,
-  notification11
+  info('Scheduled meeting will be held in 10 minutes.', { displayName: 'Zoom Meetings', key:'*'}),
+  warning('Your password will expire in 10 days.'),
+  error('E2E tests are not passing on main branch.'),
+  info('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', null),
+  warning('Quisque id arcu rhoncus, volutpat neque eu, ultricies sem.', null),
+  error('Duis dictum rhoncus laoreet.', null),
+  info('Nullam sit amet rutrum dolor.'),
+  warning('Nam aliquet eu ante non pharetra.'),
+  error('Pellentesque mattis sem quis elit condimentum vestibulum.'),
+  info('Duis fringilla nulla sapien, eget volutpat eros faucibus non.'),
+  warning('Vivamus nisi ex, tincidunt ac enim vel, consectetur suscipit lorem.'),
+  error('Nulla ornare felis sed mauris interdum consectetur.'),
 ];
+
+// {
+//     messages: ['Hey'],
+//     datetime: new Date(),
+//     initiator: { displayName: 'SYTEM', key: '*' },
+//     type: NOTIFICATION_TYPE.INFO
+//   } as NotificationModel
 
 const Template: Story<NotificationHistoryComponent> = (args) => ({
   props: args
