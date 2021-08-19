4eac28a08671d1d6c7d44677d56f80c3c26c53e7
michu-dunat <kontaktmichaldunat@gmail.com>
2021-08-19 08:56:38 +0200

    [AAE-5691] replace some notifications with ones from the past, disable ngoninit from showing in controls.

--- src/app/stories/notification-history/notification-history.stories.ts
+++ src/app/stories/notification-history/notification-history.stories.ts
@@ -1,5 +1,4 @@
-import { CoreModule, info, warning, error } from '@alfresco/adf-core';
-import { MatIconModule } from '@angular/material/icon';
+import { CoreModule, info, warning, error, NotificationModel, NOTIFICATION_TYPE } from '@alfresco/adf-core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
@@ -21,7 +20,6 @@ export default {
         CoreModule.forRoot(),
         AppCommonModule,
         BrowserAnimationsModule,
-        MatIconModule
       ]
     })
   ],
@@ -34,31 +32,52 @@ export default {
       options: ['above', 'below'],
       control: { type: 'radio' }
     },
-    notifications: { table: { disable: true } }
+    notifications: { table: { disable: true } },
+    ngOnInit: { table: { disable: true } },
   }
 } as Meta;
 
 const notifications = [
-  info('Scheduled meeting will be held in 10 minutes.', { displayName: 'Zoom Meetings', key:'*'}),
+  info('Scheduled meeting will be held in 10 minutes.', { displayName: 'Zoom Meetings', key: '*' }),
   warning('Your password will expire in 10 days.'),
-  error('E2E tests are not passing on main branch.'),
+  error('E2E tests are not passing on main branch.', null),
   info('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', null),
   warning('Quisque id arcu rhoncus, volutpat neque eu, ultricies sem.', null),
-  error('Duis dictum rhoncus laoreet.', null),
-  info('Nullam sit amet rutrum dolor.'),
-  warning('Nam aliquet eu ante non pharetra.'),
-  error('Pellentesque mattis sem quis elit condimentum vestibulum.'),
-  info('Duis fringilla nulla sapien, eget volutpat eros faucibus non.'),
-  warning('Vivamus nisi ex, tincidunt ac enim vel, consectetur suscipit lorem.'),
-  error('Nulla ornare felis sed mauris interdum consectetur.'),
+  {
+    messages: ['Morbi eget arcu ipsum.'],
+    datetime: new Date(new Date().getTime() + -5 * 60000),
+    initiator: { displayName: 'John Smith', key: '*' },
+    type: NOTIFICATION_TYPE.INFO
+  } as NotificationModel,
+  {
+    messages: ['Ut a interdum risus, vel cursus nibh.'],
+    datetime: new Date(new Date().getTime() + -61 * 60000),
+    initiator: { displayName: 'Alan Smith', key: '*' },
+    type: NOTIFICATION_TYPE.ERROR
+  } as NotificationModel,
+  {
+    messages: ['Nulla facilisi.'],
+    datetime: new Date(new Date().getTime() + -721 * 60000),
+    initiator: { displayName: 'Rachel Smith', key: '*' },
+    type: NOTIFICATION_TYPE.WARN
+  } as NotificationModel,
+  {
+    messages: ['Sed at tempus tortor.'],
+    datetime: new Date(new Date().getTime() + -1441 * 60000),
+    initiator: { displayName: 'Jessica Smith', key: '*' },
+    type: NOTIFICATION_TYPE.INFO
+  } as NotificationModel,{
+    messages: ['Sed rutrum cursus massa id pharetra.'],
+    datetime: new Date(new Date().getTime() + -4321 * 60000),
+    initiator: { displayName: 'Kate Smith', key: '*' },
+    type: NOTIFICATION_TYPE.WARN
+  } as NotificationModel,{
+    messages: ['Cras iaculis non turpis a aliquet.'],
+    datetime: new Date(new Date().getTime() + -10081 * 60000),
+    initiator: { displayName: 'Lauren Smith', key: '*' },
+    type: NOTIFICATION_TYPE.ERROR
+  } as NotificationModel,
 ];
-
-// {
-//     messages: ['Hey'],
-//     datetime: new Date(),
-//     initiator: { displayName: 'SYTEM', key: '*' },
-//     type: NOTIFICATION_TYPE.INFO
-//   } as NotificationModel
 
 const Template: Story<NotificationHistoryComponent> = (args) => ({
   props: args
