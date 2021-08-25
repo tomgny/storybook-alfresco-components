9fa3e28ec177bdd9ef5546eea8e4e1a5b1003c50
Tomasz Gnyp <wirkod@gmail.com>
2021-08-25 16:06:21 +0200

    [AAE-5687] added mock users method

--- src/app/stories/form/mock/stub-services.ts
+++ src/app/stories/form/mock/stub-services.ts
@@ -1,4 +1,4 @@
-import { GroupModel } from '@alfresco/adf-core';
+import { GroupModel, UserProcessModel } from '@alfresco/adf-core';
 import { Observable, of, ReplaySubject, Subject } from 'rxjs';
 import { GroupApiStub, ModelsApiStub } from './stub-apis';
 
@@ -43,5 +43,29 @@ export class FormServiceStub {
         { externalId: 'super admins', groups: 'super groups', id: 'super admins', name: 'Super Admins', status: 'super admins' }
       ].filter((group) => group.name.toLowerCase().startsWith(filter.toLowerCase()))
     );
+  }
+  // id
+  // email?: string;
+  //   firstName?: string;
+  //   lastName?: string;
+  //   pictureId?: number;
+  //   externalId?: string;
+  //   userImage?: string;
+
+  //id: '1', firstName: 'Leanne', lastName: 'Graham', pictureId: 1, externalId: '1', userImage: '/test'
+
+  getWorkflowUsers(_filter: string, _groupId?: string): Observable<UserProcessModel[]> {
+    return of([
+      new UserProcessModel({ id: 1, firstName: 'Leanne', lastName: 'Graham' }),
+      // new UserProcessModel({ id: 2, firstName: 'Ervin', lastName: 'Howell' }),
+      // new UserProcessModel({ id: 3, firstName: 'Clementine', lastName: 'Bauch' }),
+      // new UserProcessModel({ id: 4, firstName: 'Patricia', lastName: 'Lebsack' }),
+      // new UserProcessModel({ id: 5, firstName: 'Chelsey', lastName: 'Dietrich' }),
+      // new UserProcessModel({ id: 6, firstName: 'Dennis', lastName: 'Schulist' }),
+      // new UserProcessModel({ id: 7, firstName: 'Kurtis', lastName: 'Weissnat' }),
+      // new UserProcessModel({ id: 8, firstName: 'Nicholas', lastName: 'Runolfsdottir' }),
+      // new UserProcessModel({ id: 9, firstName: 'Glenna', lastName: 'Reichert' }),
+      // new UserProcessModel({ id: 10, firstName: 'Clementina', lastName: 'DuBuque' }),
+    ]);
   }
 }
