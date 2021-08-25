0f4d4c98528fa3470dc49001d8a86984a4ea63e7
Michał Dunat <44978358+michu-dunat@users.noreply.github.com>
2021-08-25 12:12:42 +0200

    Dev dunat aae 5686 (#23)
    * [AAE-5686] add component for edit json dialog component, add stories for it, hide controls that are not important for user
    
    * [AAE-5686] add download zip dialog component, default story for it, stub apis services and objects to make downloading work
    
    * [AAE-5686] add story for showing loading dialog
    
    * [AAE-5686] format code, move download entry to stub-data.ts file
    
    * [AAE-5686] add zip to assets, change fake node so zip will be downloaded
    
    * [AAE-5686] add methods so error about logging in wont show up anymore, change name of the fake node

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -109,6 +109,8 @@ import { CommentsComponent } from './stories/comments/comments.component';
 import { LanguageMenuComponent } from './stories/language-menu/language-menu.component';
 import { NotificationHistoryComponent } from './stories/notification-history/notification-history.component';
 import { ClipboardComponent } from './stories/clipboard/clipboard.component';
+import { EditJsonDialogComponent } from './stories/core-dialogs/edit-json-dialog/edit-json-dialog.component';
+import { DownloadZipDialogComponent } from './stories/core-dialogs/download-zip-dialog/download-zip-dialog.component';
 
 registerLocaleData(localeFr);
 registerLocaleData(localeDe);
@@ -198,6 +200,8 @@ registerLocaleData(localeSv);
     LanguageMenuComponent,
     NotificationHistoryComponent,
     ClipboardComponent,
+    EditJsonDialogComponent,
+    DownloadZipDialogComponent,
   ],
   providers: [
     { provide: AppConfigService, useClass: DebugAppConfigService },
