b81adcbaac109b9282884a5baa076eb76c352202
Michał Dunat <44978358+michu-dunat@users.noreply.github.com>
2021-09-02 09:51:25 +0200

    Dev dunat aae 5692 (#35)
    * [AAE-5692] update fake node with pagination, add pagination component, default story for it, make pagination component work by implementing event methods, add method for updating mock node, delete styles and spec files
    
    * [AAE-5692] delete unnecessary imports from stories.ts file
    
    * [AAE-5692] add infinite pagination component, default story for it, update updatePagination calls in pagination component on init and destroy, delete spec and styles files
    
    * [AAE-5692] correct spelling mistake in pagination component, delete constructor, add implementation of interface OnDestroy, add properties to infinite pagination component for docs, disable properties and methods from table
    
    * [AAE-5692] delete async keyword from method in infinite pagination component, format code
    
    * [AAE-5692] add comments for methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -116,6 +116,8 @@ import { EditJsonDialogComponent } from './stories/core-dialogs/edit-json-dialog
 import { DownloadZipDialogComponent } from './stories/core-dialogs/download-zip-dialog/download-zip-dialog.component';
 import { SidenavLayoutComponent } from './stories/layout/components/sidenav-layout/sidenav-layout.component';
 import { SidebarActionMenuComponent } from './stories/layout/components/sidebar-action-menu/sidebar-action-menu.component';
+import { PaginationComponent } from './stories/pagination/pagination.component';
+import { InfinitePaginationComponent } from './stories/infinite-pagination/infinite-pagination.component';
 import { ContextMenuComponent } from './stories/context-menu/context-menu.component';
 
 registerLocaleData(localeFr);
@@ -213,6 +215,8 @@ registerLocaleData(localeSv);
     DownloadZipDialogComponent,
     SidenavLayoutComponent,
     SidebarActionMenuComponent,
+    PaginationComponent,
+    InfinitePaginationComponent,
     ContextMenuComponent,
   ],
   providers: [
