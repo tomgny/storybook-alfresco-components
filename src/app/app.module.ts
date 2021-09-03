c09a3c02214cca972cf39c63fb32f5982ff353e1
Michał Dunat <44978358+michu-dunat@users.noreply.github.com>
2021-09-03 14:22:19 +0200

    Dev dunat aae 5792 (#41)
    * [AAE-5792] add info drawer component, default story for it, styles for text and drawer, delete spec file
    
    * [AAE-5792] add more stories, delete ngOnInit and constructor, add way to control content of tabs and whether second tab is displayed or not, comments for properties
    
    * [AAE-5792] add story for the third tab, update content text, add action and disable onClick from table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -119,6 +119,7 @@ import { SidebarActionMenuComponent } from './stories/layout/components/sidebar-
 import { PaginationComponent } from './stories/pagination/pagination.component';
 import { InfinitePaginationComponent } from './stories/infinite-pagination/infinite-pagination.component';
 import { ContextMenuComponent } from './stories/context-menu/context-menu.component';
+import { InfoDrawerComponent } from './stories/info-drawer/info-drawer.component';
 import { InfoDrawerLayoutComponent } from './stories/info-drawer-layout/info-drawer-layout.component';
 import { ErrorContentComponent } from './stories/error-content/error-content.component';
 import { EmptyContentComponent } from './stories/empty-content/empty-content.component';
@@ -222,6 +223,7 @@ registerLocaleData(localeSv);
     PaginationComponent,
     InfinitePaginationComponent,
     ContextMenuComponent,
+    InfoDrawerComponent,
     InfoDrawerLayoutComponent,
     ErrorContentComponent,
     EmptyContentComponent,
