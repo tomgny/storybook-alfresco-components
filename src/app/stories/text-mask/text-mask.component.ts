bccd0917246bb1dadd1572f3a83d2e75eafb8c64
Tomasz Gnyp <wirkod@gmail.com>
2021-09-14 12:15:32 +0200

    [AAE-5861] added text-mask component

---
+++ src/app/stories/text-mask/text-mask.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aca-text-mask',
+  templateUrl: './text-mask.component.html',
+  styleUrls: ['./text-mask.component.scss']
+})
+export class TextMaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
