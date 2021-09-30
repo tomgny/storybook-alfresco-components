ff6b20e2344f511e4870a4e08d699dd12a62710c
Tomasz Gnyp <wirkod@gmail.com>
2021-09-30 13:26:25 +0200

    [AAE-5952] added mocks, non editable task story

--- src/app/stories/process-services-cloud/task-header-cloud/task-header-cloud.component.ts
+++ src/app/stories/process-services-cloud/task-header-cloud/task-header-cloud.component.ts
@@ -2,18 +2,24 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'aca-task-header-cloud',
-  templateUrl: './task-header-cloud.component.html',
-  styleUrls: ['./task-header-cloud.component.scss']
+  templateUrl: './task-header-cloud.component.html'
 })
 export class TaskHeaderCloudComponent {
-
+  /**
+   * (Required) The name of the application.
+   */
   @Input()
   appName: string = '';
 
+  /**
+   * Show/Hide the task title
+   */
   @Input()
   showTitle: boolean = true;
 
+  /**
+   * (Required) The id of the task.
+   */
   @Input()
   taskId: string;
-
 }
--- src/app/stories/process-services-cloud/task-header-cloud/task-header-cloud.stories.ts
+++ src/app/stories/process-services-cloud/task-header-cloud/task-header-cloud.stories.ts
@@ -1,10 +1,9 @@
-import { ContentModule } from '@alfresco/adf-content-services';
 import { CoreModule, MaterialModule } from '@alfresco/adf-core';
-import { ProcessServicesCloudModule, TaskCloudModule } from '@alfresco/adf-process-services-cloud';
-import { CommonModule } from '@angular/common';
+import { ProcessServicesCloudModule, TaskCloudService } from '@alfresco/adf-process-services-cloud';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { AppCommonModule } from '../../../components/common/common.module';
+import { TaskCloudServiceStub } from './mock/stub-services';
 import { TaskHeaderCloudComponent } from './task-header-cloud.component';
 
 export default {
@@ -13,23 +12,13 @@ export default {
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [
-        CoreModule.forRoot(),
-        AppCommonModule,
-        ContentModule.forRoot(),
-        MaterialModule,
-        BrowserAnimationsModule,
-        CommonModule,
-        ProcessServicesCloudModule,
-        TaskCloudModule
-
-      ]
+      imports: [CoreModule.forRoot(), AppCommonModule, MaterialModule, BrowserAnimationsModule, ProcessServicesCloudModule],
+      providers: [{ provide: TaskCloudService, useClass: TaskCloudServiceStub }]
     })
   ],
   argTypes: {
-    links: {table: {disable: true}},
-    ngOnDestroy: {table: {disable: true}},
-    onSidenavClick: {table: {disable: true}},
+    appName: { table: { disable: true } },
+    taskId: { table: { disable: true } }
   }
 } as Meta;
 
@@ -41,7 +30,21 @@ const Template: Story<TaskHeaderCloudComponent> = (args) => ({
 
 export const Default = Template.bind({});
 Default.args = {
-  appName: 'default',
+  appName: 'taskAssigned',
   showTitle: false,
-  taskId: 'test'
-}
+  taskId: 'task'
+};
+
+export const NotEditable = Template.bind({});
+NotEditable.args = {
+  appName: 'taskCompleted',
+  showTitle: false,
+  taskId: 'task'
+};
+
+export const ShowTitle = Template.bind({});
+ShowTitle.args = {
+  appName: 'taskAssigned',
+  showTitle: true,
+  taskId: 'task'
+};
